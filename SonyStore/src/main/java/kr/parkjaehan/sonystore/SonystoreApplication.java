96d9a10564e7b6957246ec5c1c0ab124152c0f9c
ParkJaeHan <mitshowme@naver.com>
2024-10-29 17:12:26 +0900

    프로젝트 시작

---
+++ SonyStore/src/main/java/kr/parkjaehan/sonystore/SonystoreApplication.java
@@ -0,0 +1,13 @@
+package kr.parkjaehan.sonystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonystoreApplication.class, args);
+	}
+
+}
---
+++ SonyStore/src/test/java/kr/parkjaehan/sonystore/SonystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.parkjaehan.sonystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
