1d5817495a61e28e5809ac4e0d9cbb8b5742fa17
jisnu1 <jinsu4205@gmail.com>
2024-11-21 18:01:24 +0900

    박진수 : education_discount, event_detail, event, find_id_pw, login, sign_up, sign_up_agree, sign_up_input controller 작업 완료

---
+++ SonyStore/src/main/java/kr/co/sonystore/controllers/eventController.java
@@ -0,0 +1,18 @@
+package kr.co.sonystore.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class eventController {
+    
+    @GetMapping("/events/event")
+    public String event() {
+        return "events/event";
+    }
+
+    @GetMapping("/events/event_detail")
+    public String eventDetail() {
+        return "events/event_detail";
+    }
+}
---
+++ SonyStore/src/main/java/kr/co/sonystore/controllers/signController.java
@@ -0,0 +1,33 @@
+package kr.co.sonystore.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class signController {
+    
+    @GetMapping("/sign/login")
+    public String login() {
+        return "sign/login";
+    }
+
+    @GetMapping("/sign/sign_up_agree")
+    public String signUpAgree() {
+        return "sign/sign_up_agree";
+    }
+
+    @GetMapping("/sign/sign_up_input")
+    public String signUpInput() {
+        return "sign/sign_up_input";
+    }
+
+    @GetMapping("/sign/sign_up")
+    public String signUp() {
+        return "sign/sign_up";
+    }
+
+    @GetMapping("/sign/find_id_pw")
+    public String findIdPw() {
+        return "sign/find_id_pw";
+    }
+}
