f90ed1c7806639dcb4537c21e3112a832fbc882b
jisnu1 <jinsu4205@gmail.com>
2024-12-06 12:08:17 +0900

    이승현 : 마이페이지 결제내역 작업 완료

--- SonyStore/src/main/java/kr/co/sonystore/controllers/MembersController.java
+++ SonyStore/src/main/java/kr/co/sonystore/controllers/MembersController.java
@@ -1,10 +1,23 @@
 package kr.co.sonystore.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import kr.co.sonystore.helpers.WebHelper;
+import kr.co.sonystore.models.Member;
+import kr.co.sonystore.models.Payment;
+import kr.co.sonystore.services.PaymentService;
 
 @Controller
 public class MembersController {
+    @Autowired
+    private PaymentService paymentService;
+    
+    @Autowired
+    private WebHelper webHelper;
     
     @GetMapping("/members/member_modify")
     public String memberModify() {
@@ -21,7 +34,23 @@ public class MembersController {
         @SessionAttribute(value="memberInfo", required = false) Member memberInfo,
         Model model
     ) {
-        
+        if(memberInfo == null) {
+            return "/sign/login";
+        }
+
+        // 결제완료건 조회
+        Payment input = new Payment();
+        input.setMemberid(memberInfo.getMemberid());
+
+        int payComplete = 0;
+        try {
+            payComplete = paymentService.getCountPayComplete(input);
+        } catch (Exception e) {
+            webHelper.serverError(e);
+        }
+
+        model.addAttribute("payComplete", payComplete);
+
 
         return "members/my_page";
     }
