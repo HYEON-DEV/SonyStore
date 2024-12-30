b12f8af83a07c0570dec2a52a5f1735294565d19
jisnu1 <jinsu4205@gmail.com>
2024-12-30 12:02:37 +0900

    박진수 : 신규 회원 관련 테스트 완료

--- SonyStore/src/main/java/kr/co/sonystore/controllers/restfulapis/TodayMemberRestController.java
+++ SonyStore/src/main/java/kr/co/sonystore/controllers/restfulapis/TodayMemberRestController.java
@@ -2,11 +2,14 @@ package kr.co.sonystore.controllers.restfulapis;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.sonystore.helpers.RestHelper;
 import kr.co.sonystore.models.TodayMember;
-import kr.co.sonystore.models.Today_BestProduct;
 import kr.co.sonystore.services.TodayService;
-import kr.co.sonystore.services.Today_BestProductService;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -15,8 +18,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Slf4j
 @RestController
+@Tag(name="newMember API", description="주간, 월간 신규 가입자 수 API" )
 public class TodayMemberRestController {
 
     @Autowired
@@ -26,6 +30,11 @@ public class TodayMemberRestController {
 
     
     @GetMapping("/api/week_member")
+    @Operation(summary="주간 일별 신규 회원 조회", description="신규회원을 조회한다.")
+    @ApiResponses( value={
+    @ApiResponse(responseCode="200", description="주간 신규 회원 조회 성공"),
+    @ApiResponse(responseCode="500", description="주간 신규 회원 조회 실패")
+    })
     public Map<String, Object> selectWeekMember() throws Exception {
         
         List<TodayMember> output = null;
@@ -42,6 +51,11 @@ public class TodayMemberRestController {
     }
 
     @GetMapping("/api/month_member")
+    @Operation(summary="월간 주별 신규 회원 조회", description="신규회원을 조회한다.")
+    @ApiResponses( value={
+        @ApiResponse(responseCode="200", description="월간 신규 회원 조회 성공"),
+        @ApiResponse(responseCode="500", description="월간 신규 회원 조회 실패")
+        })
     public Map<String, Object> selectMonthMember() throws Exception {
         
         List<TodayMember> output = null;
