06d32def4f01c513824f64f5d3d093cb4da5a1a5
jisnu1 <jinsu4205@gmail.com>
2025-01-08 17:53:49 +0900

    박진수 : 추천제품 이미지 사이즈 변경, gnb width 1000 -> 800으로 변경, 추천제품 padding-bottom:30 삭제

--- SonyStore/src/main/java/kr/co/sonystore/controllers/RecommendController.java
+++ SonyStore/src/main/java/kr/co/sonystore/controllers/RecommendController.java
@@ -3,7 +3,9 @@ package kr.co.sonystore.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class RecommendController {
     @GetMapping("/recommend")
