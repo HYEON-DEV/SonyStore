1f58c64ee6fbc612c257e8a86a67e5a06ac7bbb4
hyeon <hyeon970315@gmail.com>
2025-02-21 19:08:11 +0900

    DESIGN: 메일 템플릿 인라인 스타일로 변경

--- SonyStore/src/main/java/kr/co/sonystore/controllers/restfulapis/OrderRestController.java
+++ SonyStore/src/main/java/kr/co/sonystore/controllers/restfulapis/OrderRestController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
